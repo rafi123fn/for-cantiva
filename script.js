354781783e9e7969b80242e1ecceffd0877c5933
rafi123fn <rafi696996@gmail.com>
2026-02-01 12:59:13 +0700

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function startSong() {
 
   document.getElementById("enterText").style.display = "none";
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const music = document.getElementById("bgMusic");
+  const enterText = document.getElementById("enterText");
+
+  enterText.addEventListener("click", () => {
+    music.play().catch(err => {
+      console.log("Autoplay blocked:", err);
+    });
+  });
+});
